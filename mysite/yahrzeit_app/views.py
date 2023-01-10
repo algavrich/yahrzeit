0602336850665fa89110e0e03888637569779ffb
Lucy Gavrich <annalgav@gmail.com>
2023-01-10 14:49:36 -0800

    Use render function for index template. Create view function and url path for create account page.

--- mysite/yahrzeit_app/views.py
+++ mysite/yahrzeit_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.template import loader
+from django.shortcuts import render
 
+#home - intro and form to calculate yahrzeit date
 def index(request):
-    return HttpResponse("Hello, world. You're at the yahrzeit index.")
+    return render(request, 'index.html')
+#create account
+def create_account(request):
+    return HttpResponse('This is create account page.')
+#login
+#user dashboard - list of decedents and dates, option to add a decedent, option to recieve email reminders
