1a96385aed47a6227f67677fd6c182aa45901934
Lucy Gavrich <annalgav@gmail.com>
2023-01-10 12:16:57 -0800

    Create data model and tables
    Co-authored-by: Eleah Miner <eleahloucks@users.noreply.github.com>

--- mysite/yahrzeit_app/models.py
+++ mysite/yahrzeit_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	username= models.CharField(max_length=30)
+	password = models.CharField(max_length=30)
+	email= models.CharField(max_length=30)
+
+	def __repr__(self):
+		return f"<User username={self.username}>"
+
+class Decedent(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	name = models.CharField(max_length=50)
+	death_date_hebrew = models.CharField(max_length=10)
+	next_date_gregorian = models.DateField()
+	
+	def __repr__(self):
+		return f"<Decedent name={self.name}>"
+	
