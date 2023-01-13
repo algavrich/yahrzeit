0c960abdda1453b80ec0873b6650710c6700b36a
Lucy Gavrich <annalgav@gmail.com>
2023-01-12 20:22:10 -0800

    Create calculate route and view to calculate next yahrzeit date
    Co-authored-by: Eleah Miner <Eleahloucks@users.noreply.github.com>

--- mysite/yahrzeit_app/urls.py
+++ mysite/yahrzeit_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create-account', views.create_account, name='create_account'),
+    path('calculate', views.calculate, name='calculate'),
 ]
--- mysite/yahrzeit_app/views.py
+++ mysite/yahrzeit_app/views.py
@@ -11,3 +11,17 @@ def create_account(request):
     return HttpResponse('This is create account page.')
 #login
 #user dashboard - list of decedents and dates, option to add a decedent, option to recieve email reminders
+#calculate
+def calculate(request):
+    decedent_name = request.POST['decedent-name']
+    decedent_date = request.POST['decedent-date']
+    # if user in session
+    if request.session.get('user_id'):
+        logged_in = True
+        # write to db
+        # display new date on dashboard
+    # else
+    else:
+        logged_in = False
+        # go to page that shows result and asks to login/create acct
+    return render()
