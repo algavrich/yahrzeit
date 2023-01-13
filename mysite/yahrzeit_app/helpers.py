da3de3465bfe545403e3810ced8035f2b6e7c9df
Lucy Gavrich <annalgav@gmail.com>
2023-01-13 14:47:06 -0800

    Finish basic get_next_date function to return tuple of next date in hebrew and gregorian forms as well as boolean value for if yahrzeit is today
    Co-authored-by: Eleah Miner <Eleahloucks@users.noreply.github.com>

--- mysite/yahrzeit_app/helpers.py
+++ mysite/yahrzeit_app/helpers.py
@@ -2,6 +2,7 @@
 
 from pyluach import dates
 from datetime import datetime
+#hebrew date has a month and day. we need to find occurance of next month and day. What is curr year
 
 def get_next_date(date_string:str):
     date_as_date = datetime.strptime(date_string, '%Y-%m-%d').date()
@@ -9,4 +10,24 @@ def get_next_date(date_string:str):
     #instead of calling it on the hebrew instance we call it on the hebrew class itself
     hebrew_date = dates.HebrewDate.from_pydate(date_as_date)
     #use .to_heb() on our gregorian date
-    print(hebrew_date)
+    # get hebrew date of today - HebrewDate.today()
+    today_h = dates.HebrewDate.today()
+    this_h_year_yahr = dates.HebrewDate(today_h.year, hebrew_date.month, hebrew_date.day)
+
+    # yahrziet hasn't passed yet this year
+    if today_h < this_h_year_yahr:
+        next_date_h = this_h_year_yahr
+        next_date_g = next_date_h.to_greg()
+        is_it_today = False
+    # yahrzeit has already passed for this year
+    elif today_h > this_h_year_yahr:
+        next_date_h = dates.HebrewDate(today_h.year + 1, hebrew_date.month, hebrew_date.day)
+        next_date_g = next_date_h.to_greg()
+        is_it_today = False
+    # yahrzeit is today
+    else:
+        next_date_h = dates.HebrewDate(today_h.year + 1, hebrew_date.month, hebrew_date.day)
+        next_date_g = next_date_h.to_greg()
+        is_it_today = True
+        
+    return (next_date_h, next_date_g, is_it_today)
