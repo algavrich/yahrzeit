e80f8b47b4e2308b4ef4fa708b341b7df630a818
Lucy Gavrich <annalgav@gmail.com>
2023-01-14 15:13:00 -0800

    Rename route that renders create account template to 'create_account_form' and write new function to actually create an account from form input values

--- mysite/yahrzeit_app/views.py
+++ mysite/yahrzeit_app/views.py
@@ -1,19 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# from django.template import loader
-from django.shortcuts import render
 from yahrzeit_app import helpers
+from yahrzeit_app import crud
 
 #home - intro and form to calculate yahrzeit date
 def index(request):
+    """Render homepage."""
     return render(request, 'index.html')
 #create account
+def create_account_form(request):
+    """Render create account form."""
+    return render(request, 'create_account.html')
+
 def create_account(request):
-    return HttpResponse('This is create account page.')
+    """Perform checks and save new account to database if checks pass."""
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+
+    if crud.create_user(username, email, password):
+        # Flash success
+        pass
+    else:
+        # Flash not success
+        pass
+
+    return redirect('index')
 #login
 #user dashboard - list of decedents and dates, option to add a decedent, option to recieve email reminders
 #calculate
 def calculate(request):
+    #TODO Should this be get since we're not writing to database?
     decedent_name = request.POST['decedent-name']
     decedent_date = request.POST['decedent-date']
     # if user in session
