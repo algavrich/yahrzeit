71145e0c87301b74bac4d54dc5ab320132fdd768
Lucy Gavrich <annalgav@gmail.com>
2023-01-14 15:11:30 -0800

    Rename route that renders template to be 'create-account-form'; 'create-account' is now the POST route for performing checks and saving new user to database

--- mysite/yahrzeit_app/urls.py
+++ mysite/yahrzeit_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create-account-form', views.create_account_form, name='create_account_form'),
     path('create-account', views.create_account, name='create_account'),
     path('calculate', views.calculate, name='calculate'),
 ]
