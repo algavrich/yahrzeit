04589e5ddfdaa0e0f169440359efa06df9d41ebe
Lucy Gavrich <annalgav@gmail.com>
2023-01-16 15:11:02 -0800

    Create wrapper function to handle DoesNotExist exceptions for User queries, remove get_user_by_username and username parameter from create_user because username was removed from model.

--- mysite/yahrzeit_app/crud.py
+++ mysite/yahrzeit_app/crud.py
@@ -3,17 +3,32 @@
 from yahrzeit_app.models import User
 
 
-def create_user(username: str, password: str, email: str) -> bool:
+def create_user(email: str, password: str) -> bool:
     """Instantiate a User with given attributes and save to database."""
 
-    if not get_user_by_email(email) and not get_user_by_username(username):
-        new_user = User(username=username, password=password, email=email)
+    if not get_user_by_email(email):
+        new_user = User(password=password, email=email)
         new_user.save()
         return True
     else:
         return False
 
 
+def safe_get(func):
+    """Decorator to handle DoesNotExist exceptions for queries."""
+
+    def wrapper_safe_get(*args, **kwargs):
+        """Wrapper to handle DoesNotExist exceptions for queries."""
+
+        try:
+            return func(*args, **kwargs)
+        except User.DoesNotExist:
+            return None
+
+    return wrapper_safe_get
+
+
+@safe_get
 def get_user_by_email(email:str) -> User:
     """Retrieve a User with given email if it exists.
     Otherwise, return None.
@@ -22,12 +37,4 @@ def get_user_by_email(email:str) -> User:
 
     return User.objects.filter(email=email).first()
 
-
-def get_user_by_username(username:str) -> User:
-    """Retrieve a User with given username if it exists.
-    Otherwise, return None.
     
-    """
-
-    return User.objects.filter(username=username).first()
-
