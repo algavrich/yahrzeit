56b45868c77837119303a2dbac18401cdfecc68a
Lucy Gavrich <annalgav@gmail.com>
2023-03-02 12:21:30 -0800

    Write adjust_adar_leap for use in get_next_date and get_following_dates. Write today_date_string for use in restricting date input to past dates in index template. Delete pseudocode comments and format code for readability.

--- mysite/yahrzeit_app/helpers.py
+++ mysite/yahrzeit_app/helpers.py
@@ -16,65 +16,67 @@ def date_from_string(date_string: str) -> datetime.date:
 
     return datetime.strptime(date_string, '%Y-%m-%d').date()
 
-#converting a gregorian string to a hebrew string
-def greg_to_heb(greg_str):
-    #parse date
-    #convert to hebrew date obj
+
+def greg_to_heb(greg_str: str) -> str:
+    """Convert gregorian date string to hebrew date string."""
+
     return h_date_stringify_db(
         dates.HebrewDate.from_pydate(
-            date_from_string(greg_str)
+            date_from_string(greg_str),
         )
     )
 
-#hebrew date has a month and day. we need to find occurance of next month and day. What is curr year
+
+def adjust_adar_leap(month:int, year: int) -> int:
+    """Return yahrzeit month adjusted for future year's leap status."""
+
+    if (month == 13 and not hebrewcal.Year(year).leap):
+        return 12
+    
+    return month
+
+
+def today_date_string() -> str:
+    """Return string of today's date."""
+
+    return date.strftime(date.today(), '%Y-%m-%d')
+
+
 def get_next_date(date_string:str, after_sunset: bool) -> Tuple:
+    """Calculate next yahrzeit date."""
+
     date_as_date = date_from_string(date_string)
-    #instantiate a gregorian date with our values
-    #instead of calling it on the hebrew instance we call it on the hebrew class itself
     hebrew_date = dates.HebrewDate.from_pydate(date_as_date)
-    print(hebrew_date)
-    print(after_sunset)
+
     if after_sunset:
         hebrew_date = hebrew_date.add(days=1)
-    print(hebrew_date)
-    future_month = hebrew_date.month
-    #use .to_heb() on our gregorian date
-    # get hebrew date of today - HebrewDate.today()
+  
     today_h = dates.HebrewDate.today()
-    # if hebrew_date was leap month and this year doesn't have leap, make it month 12
-    if (hebrewcal.Year(hebrew_date.year).leap
-        and not hebrewcal.Year(today_h).leap):
-        future_month = 12
         
     this_h_year_yahr = dates.HebrewDate(
         today_h.year,
-        future_month,
+        adjust_adar_leap(hebrew_date.month, today_h.year),
         hebrew_date.day,
     )
 
-    # yahrziet hasn't passed yet this year
     if today_h < this_h_year_yahr:
         next_date_h = this_h_year_yahr
         next_date_g = next_date_h.to_greg()
         is_it_today = False
-    # yahrzeit has already passed for this year
-    elif today_h > this_h_year_yahr:
-        next_date_h = dates.HebrewDate(
-            today_h.year + 1,
-            hebrew_date.month,
-            hebrew_date.day,
-        )
-        next_date_g = next_date_h.to_greg()
-        is_it_today = False
-    # yahrzeit is today
+
     else:
         next_date_h = dates.HebrewDate(
             today_h.year + 1,
-            hebrew_date.month,
+            adjust_adar_leap(hebrew_date.month, today_h.year+1),
             hebrew_date.day,
         )
         next_date_g = next_date_h.to_greg()
-        is_it_today = True
+
+        if today_h > this_h_year_yahr:
+            is_it_today = False
+
+        else:
+            is_it_today = True
 
     return (next_date_h, next_date_g, is_it_today)
 
@@ -112,26 +114,20 @@ def get_following_dates(
 
     """
 
-    #create empty dict for dates
     following_dates = {}
 
-    #iterate the number of times for the number of years requested
     for num in range(1, num_years+1):
-        #within each iteration, add that number to the year - hebrew
         following_date_h = dates.HebrewDate(
             next_date_h.year+num,
-            next_date_h.month,
+            adjust_adar_leap(next_date_h.month, next_date_h.year+num),
             next_date_h.day,
         )
-        #convert date to gregorian
         following_date_g = following_date_h.to_greg()
 
-        #turn dates to strings and add to dict
         following_dates[
             h_date_stringify_res(following_date_h)
         ] = g_date_stringify_res(following_date_g)
 
-    #return dict
     return following_dates
 
 
@@ -170,13 +166,15 @@ def get_sunset_time_helper(
         coordinates['lat'],
         coordinates['lng'],
     )
+
     try:
         sunset = sun.sunset(
             location.observer,
             date_from_string(date_string),
             tz,
         )
+        
     except ValueError:
         return
 
-    return (date.strftime(sunset, '%-I:%M %p'))
+    return (datetime.strftime(sunset, '%-I:%M %p'))
