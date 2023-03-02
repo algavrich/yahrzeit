3f9d9c696b5b199620bcf475e26b386fe4b947cf
Lucy Gavrich <annalgav@gmail.com>
2023-03-01 17:17:19 -0800

    Add unique constraint to email column of User table

--- mysite/yahrzeit_app/models.py
+++ mysite/yahrzeit_app/models.py
@@ -2,11 +2,11 @@
 
 from django.db import models
 
-# Create your models here.
+
 class User(models.Model):
 	"""Model for a User."""
 
-	email= models.EmailField(max_length=100)
+	email= models.EmailField(max_length=100, unique=True)
 	password = models.CharField(max_length=30)
 
 	def __repr__(self) -> str:
