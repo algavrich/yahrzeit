92b832730a6dd433726889e0bf1539ca3e2039b9
Lucy Gavrich <annalgav@gmail.com>
2023-03-02 13:51:55 -0800

    Create event listener on create account form to override default form submission and validate password before sending AJAX request to create_account view

---
+++ mysite/yahrzeit_app/static/js/create_account.js
@@ -0,0 +1,44 @@
+'use strict';
+
+document.querySelector('.calculator-form').addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const retypePassword = document.getElementById('retype-password').value;
+
+    if (password === retypePassword) {
+        if (password.match(/[A-Z]/)
+        && password.match(/[a-z]/)
+        && password.match(/[0-9]/)
+        && password.match(/[!@#$%\^&\*\(\)-_=\+\[\]\{\}\\\|;:'"\,<\.>\/\?~`]/)
+        && password.length >= 8) {
+            const csrfToken = Cookies.get('csrftoken');
+            console.log(csrfToken);
+            fetch('api/create-account', {
+                method: 'POST',
+                body: JSON.stringify({
+                    'email': email,
+                    'password': password,
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken,
+                },
+            }).then((res) => {
+                return res.json()
+            }).then((resData) => {
+                if (resData.status === 'success') {
+                    console.log('success')
+                    window.location.href = '/yahrzeit/dashboard';
+                } else {
+                    alert('That email is already associated with an account');
+                }
+            });
+        } else {
+            alert('password doesn\'t meet requirements');
+        }
+    } else {
+        alert('passwords don\'t match');
+    }
+});
