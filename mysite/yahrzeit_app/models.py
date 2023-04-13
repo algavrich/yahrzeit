1bc6b01ce108b2a788f6d47a3fbed6a7f8f3ef80
Lucy Gavrich <annalgav@gmail.com>
2023-04-13 10:37:01 -0700

    Create CustomUser and CustomUserManager models for customization of django user authentication framework. Specifically, login credentials will rely on email address as unique 'username' field so that an actual username is not necessary.

--- mysite/yahrzeit_app/models.py
+++ mysite/yahrzeit_app/models.py
@@ -1,30 +1,56 @@
 """Models for yahrzeit app database."""
 
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
-class User(models.Model):
-	"""Model for a User."""
+class CustomUserManager(BaseUserManager):
+    """Model for a UserManager."""
 
-	email= models.EmailField(max_length=100, unique=True)
-	password = models.CharField(max_length=200)
+    def create_user(self, email, password):
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-	def __repr__(self) -> str:
-		"""String representation for User."""
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
-		return f"<User email={self.email}>"
+
+class CustomUser(AbstractBaseUser):
+    """Model for a User."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self) -> str:
+        """String representation for User."""
+
+        return f"<User email={self.email}>"
+
+    @property
+    def is_staff(self):
+        return self.is_admin
 
 
 class Decedent(models.Model):
-	"""Model for a Decedent."""
+    """Model for a Decedent."""
 
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	name = models.CharField(max_length=50)
-	death_date_hebrew = models.CharField(max_length=10)
-	next_date_hebrew = models.CharField(max_length=10)
-	next_date_gregorian = models.CharField(max_length=10)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    death_date_hebrew = models.CharField(max_length=10)
+    next_date_hebrew = models.CharField(max_length=10)
+    next_date_gregorian = models.CharField(max_length=10)
 
-	def __repr__(self) -> str:
-		"""String representation for Decedent."""
+    def __str__(self) -> str:
+        """String representation for Decedent."""
 
-		return f"<Decedent name={self.name}>"
+        return f"<Decedent name={self.name}>"
